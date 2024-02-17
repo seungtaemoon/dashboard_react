1ff8b4214d7716091530061e309e3310d19ee69c
seungtaemoon <Robbie@email.com>
2024-02-17 15:49:50 +0900

    adjusted search bar UI

--- dashboard/src/components/Content.jsx
+++ dashboard/src/components/Content.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import ContentHeader from "./ContentHeader";
+import "../styles/content.css"
 
 const Content = () => {
     return <div className="content">
