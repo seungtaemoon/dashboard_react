f3c3369a7382eb7d1ebfb32c0b31859d9521f2f5
seungtaemoon <Robbie@email.com>
2024-03-16 18:18:16 +0900

    cart UI update

--- dashboard/src/components/Cart.jsx
+++ dashboard/src/components/Cart.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addItem, deleteItem } from "../actions/cartAction";
+
 
 const Cart = () => {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  console.log("store", state); // store내 저장된 상태정보 읽어오기
   return (
     <div className="cart">
-      <h2>Number of items in Cart:</h2>
-      <button className="green">Add Item to Cart</button>
-      <button className="red">Remove Item from Cart</button>
+      <h2>Number of items in Cart: {state.numOfItems}</h2>
+      <button className="green"
+        onClick={() => {
+          dispatch(addItem());
+        }}
+      >
+        Add Item to Cart
+      </button>
+      <button className="red"
+              disabled={state.numOfItems > 0 ? false : true}
+              onClick={() => {
+                dispatch(deleteItem());
+              }}
+      >
+        Remove Item from Cart
+      </button>
     </div>
   );
 };
